943da78a4117b29644a0b23042715c96afc1091c
Sangho <sanghapriya.bose@gmail.com>
2026-07-26 22:22:53 +0100

    change directory name

--- src/main/java/com/sangatwork/learngateway/report/ReportApplication.java
+++ src/main/java/com/sangatwork/learngateway/report/ReportApplication.java
@@ -1,4 +1,4 @@
-package com.sangatwork.learngateway.report.report;
+package com.sangatwork.learngateway.report;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
